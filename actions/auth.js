f3b97d5045612e63a5d9c10660773d039cc42fc3
cardfaux <cardfaux@tutanota.com>
2020-12-03 12:13:38 -0600

    removed unused code from the auth.js file

--- actions/auth.js
+++ actions/auth.js
@@ -17,21 +17,6 @@ export const signup = (user) => {
     .catch((err) => console.log(err));
 };
 
-// export const signup = (user) => {
-//   return fetch('http://localhost:8000/api/signup', {
-//     method: 'POST',
-//     headers: {
-//       Accept: 'application/json',
-//       'Content-Type': 'application/json',
-//     },
-//     body: JSON.stringify(user),
-//   })
-//     .then((response) => {
-//       return response.json();
-//     })
-//     .catch((err) => console.log(err));
-// };
-
 export const signin = (user) => {
   return fetch(`${API}/signin`, {
     method: 'POST',
