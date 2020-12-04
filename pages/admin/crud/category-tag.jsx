b225aee971d01165109102f172e30a9be71389a5
cardfaux <cardfaux@tutanota.com>
2020-12-04 16:26:27 -0600

    changed manage categories and tags to manage categories and manage tags

--- pages/admin/crud/category-tag.jsx
+++ pages/admin/crud/category-tag.jsx
@@ -11,13 +11,13 @@ const CategoryTag = () => {
       <Admin>
         <div className='container-fluid'>
           <div className='row'>
-            <div className='col-md-12 pt-5 pb-5'>
-              <h2>Manage Categories and Tags</h2>
-            </div>
+            <div className='col-md-12 pt-5 pb-5'></div>
             <div className='col-md-6'>
+              <h2>Manage Categories</h2>
               <Category />
             </div>
             <div className='col-md-6'>
+              <h2>Manage Tags</h2>
               <Tag />
             </div>
           </div>
