a278874d63e52cd42df4dcc086e984c6be76f073
cardfaux <cardfaux@tutanota.com>
2020-12-06 11:31:27 -0600

    added categories checked to the state

--- components/crud/BlogCreate.jsx
+++ components/crud/BlogCreate.jsx
@@ -27,6 +27,9 @@ const CreateBlog = ({ router }) => {
   const [categories, setCategories] = useState([]);
   const [tags, setTags] = useState([]);
 
+  const [checked, setChecked] = useState([]); // categories
+  const [checkedTag, setCheckedTag] = useState([]); // tags
+
   const [body, setBody] = useState(blogFromLS());
   const [values, setValues] = useState({
     error: '',
@@ -86,12 +89,28 @@ const CreateBlog = ({ router }) => {
     }
   };
 
+  const handleToggle = (c) => () => {
+    setValues({ ...values, error: '' });
+    // return the first index or -1
+    const clickedCategory = checked.indexOf(c);
+    const all = [...checked];
+
+    if (clickedCategory === -1) {
+      all.push(c);
+    } else {
+      all.splice(clickedCategory, 1);
+    }
+    console.log(all);
+    setChecked(all);
+    formData.set('categories', all);
+  };
+
   const showCategories = () => {
     return (
       categories &&
       categories.map((c, i) => (
         <li key={i} className='list-unstyled'>
-          <input type='checkbox' className='mr-2' />
+          <input onChange={handleToggle(c._id)} type='checkbox' className='mr-2' />
           <label className='form-check-label'>{c.name}</label>
         </li>
       ))
